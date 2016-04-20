7e4295e59115d2c77f2bce4b45e1e1478a5ffc36
achandi <chandi.ajit@gmail.com>
2016-04-20 12:34:59 -0400

    Sports table creation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160420144251) do
+ActiveRecord::Schema.define(version: 20160420162428) do
+
+  create_table "sports", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
